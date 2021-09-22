6f680823d0db9cfcbc670e112a820d137f4fa2f8
Simonas Nareckas <simasn@gmail.com>
2021-09-22 10:28:34 +0300

    Replacing zprezto with starship prompt

--- zsh/install.sh
+++ zsh/install.sh
@@ -1,13 +1,7 @@
 #! /usr/bin/env zsh
 
-rm -fr "${ZDOTDIR:-$HOME}/.zprezto"
-git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
-
-setopt EXTENDED_GLOB
-for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md; do
-  ln -sfn "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
-done
+brew install starship
 
 ln -sFi "$PWD/config" "$HOME/.config/zsh"
 ln -sfn "$PWD/zshrc" "$HOME/.zshrc"
-ln -sfn "$PWD/zpreztorc" "$HOME/.zpreztorc"
+ln -sFi "$PWD/starship.toml" "$HOME/.config/starship.toml"
